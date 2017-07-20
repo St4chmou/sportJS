7e2e7b0f3feccac09185c6668cb6b48b29491c19
jbayart <julien.bayart@soprasteria.com>
2017-07-20 15:18:16 +0200

    TP observables

--- src/app/stat/stat.component.ts
+++ src/app/stat/stat.component.ts
@@ -1,9 +1,9 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {Observable} from "rxjs/Observable";
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Observable } from "rxjs/Observable";
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/takeUntil';
 import 'rxjs/add/operator/mergeMap';
-import {ResizeGraphService} from "./visualization/resize-graph.service";
+import { ResizeGraphService } from "./visualization/resize-graph.service";
 
 @Component({
   selector: 'sp-stat',
@@ -18,23 +18,36 @@ export class StatComponent implements OnInit {
   @ViewChild('rightBlock') rightBlock: ElementRef;
 
   constructor(private elementRef: ElementRef,
-              private resizeGraphService: ResizeGraphService) { }
+    private resizeGraphService: ResizeGraphService) { }
 
   ngOnInit() {
 
-    // TODO exo-obs
+    const mouseDown$ = Observable.fromEvent(this.separator.nativeElement, 'mousedown');
+    const mouseUp$ = Observable.fromEvent(document, 'mouseup');
+    const mouseMove$ = Observable.fromEvent(document, 'mousemove');
+
+    mouseDown$
+    .mergeMap(e => mouseMove$.takeUntil(mouseUp$))
+    .subscribe((e: MouseEvent) => {
+      const widths = this.getBlocksWidth(e.x);
+      this.leftBlock.nativeElement.setAttribute('style', `width:${widths.leftWidth}px`);
+      this.rightBlock.nativeElement.setAttribute('style', `width:${widths.rightWidth}px`);
+      this.resizeGraphService.setWidth(blocksWidth.rightWidth);
+    });
 
     const separatorRec = this.separator.nativeElement.getBoundingClientRect();
     const blocksWidth = this.getBlocksWidth(separatorRec.left + separatorRec.width / 2);
     this.resizeGraphService.setWidth(blocksWidth.rightWidth);
+
+
   }
 
-  private getBlocksWidth(mouseX: number): {leftWidth, rightWidth} {
+  private getBlocksWidth(mouseX: number): { leftWidth, rightWidth } {
     const rectContainer = this.elementRef.nativeElement.getBoundingClientRect();
     const marginAndPaddingSeparator = 7;
     const leftWidth = mouseX - rectContainer.left - marginAndPaddingSeparator;
     const rightWidth = rectContainer.width + rectContainer.left - mouseX - marginAndPaddingSeparator;
-    return {leftWidth, rightWidth};
+    return { leftWidth, rightWidth };
   }
 
 }
--- src/app/stat/visualization/synthesis/synthesis.component.ts
+++ src/app/stat/visualization/synthesis/synthesis.component.ts
@@ -1,13 +1,15 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {RecordStoreService} from "../../record-store.service";
-import {Observable} from "rxjs/Observable";
-import {Record} from "../../../shared/record/record";
+import { Subscription } from 'rxjs/Subscription';
+import { HeartBeat } from './../../../shared/record/heart-beat';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RecordStoreService } from "../../record-store.service";
+import { Observable } from "rxjs/Observable";
+import { Record } from "../../../shared/record/record";
 import 'rxjs/add/operator/reduce'
 import 'rxjs/add/operator/max'
 import 'rxjs/add/operator/min'
 import 'rxjs/add/operator/last'
 import 'rxjs/add/observable/from'
-import {Subject} from "rxjs/Subject";
+import { Subject } from "rxjs/Subject";
 
 @Component({
   selector: 'sp-synthesis',
@@ -24,25 +26,56 @@ export class SynthesisComponent implements OnInit, OnDestroy {
     this.record$ = this.recordStoreService.getSelectedRecord$();
   }
 
-  ngOnDestroy(): void { }
+  ngOnDestroy(): void {
+  }
 
   getType$(): Observable<string> {
-    return null;
+    return this.record$
+      .filter(record => !!record)
+      .map(record => {
+        return record.type;
+      })
   }
 
   getDuration$(): Observable<string> {
-    return null;
+    return this.record$
+      .filter(record => !!record)
+      .mergeMap(record => Observable.from(record.heartBeats).last())
+      .map(heartBeat => {
+        return heartBeat.x.toString() + ' secondes';
+      });
   }
 
   getMax$(): Observable<number> {
-    return null;
+    return this.record$
+      .filter(record => !!record)
+      .mergeMap(record => Observable.from(record.heartBeats)
+        .map(heartBeat => heartBeat.y)
+        .max())
   }
 
   getMin$(): Observable<number> {
-    return null;
+    return this.record$
+      .filter(record => !!record)
+      .mergeMap(record => Observable.from(record.heartBeats)
+        .map(heartBeat => heartBeat.y)
+        .min()
+      )
   }
 
   getAverage$(): Observable<number> {
-    return null
+    return this.record$
+      .filter(record => !!record)
+      .mergeMap(record => Observable.from(record.heartBeats)
+        .map(heartBeat => heartBeat.y)
+        .reduce(cumulPourMoyenne, { somme: 0, nombreElement: 0 })
+        .map(cumul => cumul.somme / cumul.nombreElement));
+
+    function cumulPourMoyenne(cumul, y) {
+      return {
+        somme: cumul.somme + y,
+        nombreElement: cumul.nombreElement + 1
+      };
+    }
   }
 }
